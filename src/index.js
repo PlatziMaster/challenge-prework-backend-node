7d6681167780d5b816995b2b7b54f71e136effd8
CarGDev <ingecarlos.gutierrez@gmail.com>
2020-08-14 17:14:13 -0500

    Finishing challenge

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const dotenv = require('dotenv').config()
-
-const { ENV } = process.env
+const products = require('./assets/js/products')
+const { ENV , PORT } = process.env
 const express = require("express"),
   path = require("path"),
   app = express(),
-  port = process.env.PORT || 3000,
+  port = process.env.PORT || PORT,
   development = 'development';
 
 if (ENV === development) {
@@ -22,8 +22,94 @@ app.get('/receipts', (req, res) => {
 });
 
 app.get('/products', (req, res) => {
-  let storeProducts = '';
-  res.json(storeProducts);
+  let storeProducts = products.mockCheck();
+  res.status(200).json({
+    message: 'Products listed',
+    data: storeProducts
+  })
+});
+
+app.get('/products/:id', (req, res) => {
+  try {
+    const { id } = req.params
+    let product = products.mockCheck()[id - 1].Product;
+    let cat = products.mockCheck()[id - 1].Category;
+    let desc = products.mockCheck()[id - 1].Description;
+    let price = products.mockCheck()[id - 1].Price;
+    let qty = products.mockCheck()[id - 1].Qty;
+    let sku = products.mockCheck()[id - 1].Sku;
+    let img = products.mockCheck()[id - 1].Image;
+    let place = products.mockCheck()[id - 1]["Transaction Type"];
+    res.status(200).json({
+      Message: 'Products found',
+      Product: product,
+      Category: cat,
+      Description: desc,
+      Price: price,
+      Quantity: qty,
+      Sku: sku,
+      Img: img,
+      Place: place,
+    })
+  } catch (err) {
+    res.status(404).json({
+      Message: 'Not products found'
+    })
+  }
+});
+
+app.post('/products/create', (req, res) => {
+  let id = randomNum()
+  let product = products.mockCheck()[id].Product;
+  let cat = products.mockCheck()[id].Category;
+  let desc = products.mockCheck()[id].Description;
+  let price = products.mockCheck()[id].Price;
+  let qty = products.mockCheck()[id].Qty;
+  let sku = products.mockCheck()[id].Sku;
+  let img = products.mockCheck()[id].Image;
+  let place = products.mockCheck()[id]["Transaction Type"];
+  res.status(200).json({
+    message: 'Product created',
+    Product: product,
+    Category: cat,
+    Description: desc,
+    Price: price,
+    Quantity: qty,
+    Sku: sku,
+    Img: img,
+    Place: place,
+  })
+});
+
+app.put('/products/update', (req, res) => {
+  let id = randomNum()
+  let product = products.mockCheck()[id].Product;
+  let cat = products.mockCheck()[id].Category;
+  let desc = products.mockCheck()[id].Description;
+  let price = products.mockCheck()[id].Price;
+  let qty = products.mockCheck()[id].Qty;
+  let sku = products.mockCheck()[id].Sku;
+  let img = products.mockCheck()[id].Image;
+  let place = products.mockCheck()[id]["Transaction Type"];
+  res.status(201).json({
+    message: 'Product updated',
+    Product: product,
+    Category: cat,
+    Description: desc,
+    Price: price,
+    Quantity: qty,
+    Sku: sku,
+    Img: img,
+    Place: place,
+  })
+});
+
+app.delete('/products/delete', (req, res) => {
+  let id = randomNum()
+  let product = products.mockCheck()[id].Product;
+  res.status(200).json({
+    message: `Product --${product}-- has been deleted`,
+  })
 });
 
 app.listen(port, err => {
@@ -33,3 +119,7 @@ app.listen(port, err => {
   }
   console.log(`Listening http://localhost:${port}`);
 });
+
+function randomNum () {
+  return  Math.floor(Math.random() * 1001);
+}
