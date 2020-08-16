0a9fbe23ea67f2130e2ac5ab949640e7cb233fd6
Daniel Santos <dsantos0495@gmail.com>
2020-08-16 09:40:34 -0500

    Creamos varible de entorno, instalamos nodemon, debuggeamos, hicimos el mock y rutas de PS

--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
 const express = require("express"),
   path = require("path"),
+  fetch = require('node-fetch'),
   app = express(),
   port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+  app.get('/', (req, res) => {
   let userInfo = req.header("user-agent");
   res.send(`UserInfo: ${userInfo}`);
 });
 
 app.get('/receipts', (req, res) => {
-  let file = path.join(__dirname, "asset/receipt.pdf");
-  res.sendFile();
+  let file = path.join(__dirname, "assets/receipt.pdf");
+  res.sendFile(file);
 });
 
-app.get('/products', (req, res) => {
-  let storeProducts = '';
-  res.json(storeProducts);
+app.get('/products', async (req, res) => {
+  let storeProducts = 'https://92413e28-70f6-415d-9a22-35021dbb85f6.mock.pstmn.io';
+  try {
+
+    let response = await fetch(storeProducts);
+    let data = await response.json();
+    res.json(data);
+  } catch(error) {
+    res.json(error.message);
+  }
 });
 
 app.listen(port, err => {
