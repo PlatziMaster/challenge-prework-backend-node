dc68598d4466be25396b3d60876d3880ba353dfe
Bryan Alejandro Sanchez Ramirez <sarabryanale@outlook.com>
2020-08-15 19:47:09 -0500

    Adding routes and mock for products

--- src/index.js
+++ src/index.js
@@ -1,24 +1,33 @@
+const { products, getProducts } = require("./products");
+
 const express = require("express"),
   path = require("path"),
   app = express(),
   port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   let userInfo = req.header("user-agent");
   res.send(`UserInfo: ${userInfo}`);
 });
 
-app.get('/receipts', (req, res) => {
-  let file = path.join(__dirname, "asset/receipt.pdf");
-  res.sendFile();
+app.get("/receipts", (req, res) => {
+  let file = path.join(__dirname, "assets/receipt.pdf");
+  res.sendFile(file);
 });
 
-app.get('/products', (req, res) => {
-  let storeProducts = '';
+app.get("/products", (req, res) => {
+  let storeProducts = products;
   res.json(storeProducts);
 });
 
-app.listen(port, err => {
+app.get("/products/:id", (req, res) => {
+  const { id } = req.params;
+  const result = getProducts(id);
+
+  res.json(result);
+});
+
+app.listen(port, (err) => {
   if (err) {
     console.error("Error: ", err);
     return;
