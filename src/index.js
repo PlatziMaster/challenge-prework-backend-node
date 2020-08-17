5dd02a624f1e8cfa6844b8564a1b8370b270483c
jesusleonardo.garcia <jesusleonardo.garcia@bbva.com>
2020-08-17 10:56:36 -0500

    se incluye los mocks el archivo .env(ignorado por .gitignore) y se crean las rutas para el CRUD, se corrige los typos para mostrar el pdf

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
+const dotenv = require('dotenv')
+  dotenv.config()
+
 const express = require("express"),
   path = require("path"),
   app = express(),
   port = process.env.PORT || 3000;
+
+const { productsMock } = require('../utils/mocks/mockProducts')
 
 app.get('/', (req, res) => {
   let userInfo = req.header("user-agent");
@@ -9,14 +14,72 @@ app.get('/', (req, res) => {
 });
 
 app.get('/receipts', (req, res) => {
-  let file = path.join(__dirname, "asset/receipt.pdf");
-  res.sendFile();
+  let file = path.join(__dirname, "assets/receipt.pdf");
+  res.sendFile(file);
 });
 
+//list products
 app.get('/products', (req, res) => {
-  let storeProducts = '';
-  res.json(storeProducts);
-});
+  res.status(200).json({
+    data: productsMock,
+    message: 'products listed'
+  });
+})
+
+//list a product by id
+app.get('/products/:id', (req, res) => {
+  let consult = productsMock.filter( (a) => {
+    return a.id == req.params.id
+  })
+  res.status(200).json({
+    data: consult,
+    message: 'Product retriev'
+  });
+})
+
+//create a product
+app.post('/products/', (req, res) => {
+  let id_= productsMock[productsMock.length - 1].id + 1
+  let createProduct = {
+    "id": id_,
+    "name_product": "Eggplant - Baby",
+    "description": "Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.\n\nDonec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.\n\nDuis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.",
+    "value": "$6.68",
+    "quantity": 1
+  }
+  res.status(201).json({
+    data: createProduct,
+    message: "Product created"
+  })
+})
+
+//update a product
+app.put('/products/:id', (req, res) => {
+  let consult = productsMock.filter( (a) => {
+    return a.id == req.params.id
+  })
+  consult.name_product = "Product modified"
+
+  res.status(200).json({
+    data: consult,
+    message: 'Product updated'
+  });
+})
+
+//delete a product by id
+app.delete('/products/:id', (req, res) => {
+  for (var i = 0; i < productsMock.length; i++) {
+    if (productsMock[i].id == req.params.id) {
+      productsMock.splice(i, 1);
+      break;
+    }
+  }
+  
+  res.status(200).json({
+    data: productsMock,
+    message: 'Product deleted'
+  });
+})
 
 app.listen(port, err => {
   if (err) {
