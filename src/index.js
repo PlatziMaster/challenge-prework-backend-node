4d0111e7a4ad38941993a3c5279ca85facd69291
DevTop <jupeto@hotmail.es>
2020-08-17 09:47:53 -0500

    Reto cumplido hasta el punto 2, falta punto 3

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
+require('dotenv').config();
+
 const express = require("express"),
   path = require("path"),
   app = express(),
-  port = process.env.PORT || 3000;
+  port = process.env.PORT;
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => {  
   let userInfo = req.header("user-agent");
-  res.send(`UserInfo: ${userInfo}`);
+  console.log(userInfo)
+  res.send(`UserInfo: ${userInfo}`);  
 });
 
 app.get('/receipts', (req, res) => {
-  let file = path.join(__dirname, "asset/receipt.pdf");
-  res.sendFile();
+  let file = path.join(__dirname, "assets/receipt.pdf");
+  res.sendFile(file);
 });
 
 app.get('/products', (req, res) => {
