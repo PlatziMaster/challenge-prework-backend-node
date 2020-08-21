3393c038908d30cfa58630d28443d95def4f1ab8
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2020-08-21 13:14:39 -0500

    Show correctly all routes to products

--- src/server/server.js
+++ src/server/server.js
@@ -1,12 +1,35 @@
 const express = require('express');
-
 const app = express();
 
-app.get('*', () => {
-  res.send('Hello world');
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('LANDING PAGE');
 });
+
+app.get('/products', (req, res) => {
+  res.json({
+    products: ['ARRAY WITH SOME PRODUCTS']
+  });
+});
+
+app.post('/products/:id', (req, res) => {
+  console.log(req.body);
+  console.log(req.params);
+  res.send('SE CREA UN NUEVO PRODUCTO');
+});
+
+app.put('/products/:id', (req, res) => {
+  console.log(req.body);
+  res.send(`Product ${req.params.id} updated.`);
+});
+
+app.delete('/products/:id', (req, res) => {
+  res.send(`Product ${req.params.id} deleted.`);
+});
+
 
 app.listen(4000, (err) => {
   if(err) { console.error(err); }
   else { console.log('Server is running PORT 4000') } 
-})
+});
