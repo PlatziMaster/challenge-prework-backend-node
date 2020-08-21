894d4ce0958dea7225332f227f0024673b00f980
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2020-08-20 20:30:47 -0500

    Fix path /recipts and check the PDF is showed

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ app.get('/', (req, res) => {
 });
 
 app.get('/receipts', (req, res) => {
-  let file = path.join(__dirname, "asset/receipt.pdf");
-  res.sendFile();
+  let file = path.join(__dirname, "assets/receipt.pdf");
+  res.sendFile(file);
 });
 
 app.get('/products', (req, res) => {
