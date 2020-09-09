d28a59126ed83cffca0063cd9012c339395051ad
Juan Rios <mrdaniel19996@gmail.com>
2020-09-09 12:26:01 -0500

    [UPDATE] Challenge done

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
 const express = require("express"),
-  path = require("path"),
   app = express(),
   port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  let userInfo = req.header("user-agent");
-  res.send(`UserInfo: ${userInfo}`);
-});
+var Routes = require('./routes')
 
-app.get('/receipts', (req, res) => {
-  let file = path.join(__dirname, "asset/receipt.pdf");
-  res.sendFile();
-});
 
-app.get('/products', (req, res) => {
-  let storeProducts = '';
-  res.json(storeProducts);
-});
+app.use('/', Routes)
+
+
 
 app.listen(port, err => {
   if (err) {
---
+++ src/mock/db.js
@@ -0,0 +1,24 @@
+const products = [
+    {
+      id: '1',
+      product: 'camisa',
+      price: '150'
+    },
+    {
+      id: '2',
+      product: 'boton',
+      price: '50'
+    },
+    {
+      id: '3',
+      product: 'sticker',
+      price: '10'
+    },
+  ]
+
+
+
+
+module.exports = {
+  products,
+}
